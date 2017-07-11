5c7716966c8099cdb9ecbef113aa429cf967518b
Jack Wood <gritteh@gmail.com>
2017-07-11 16:59:24 +0100

    myportfolio

---
+++ js/myportfolio.js
@@ -0,0 +1,19 @@
+/*$(document).ready(function() {
+  var im1 = document.getElementById("im1");
+  var im2 = document.getElementById("im2");
+  var im3 = document.getElementById("im3");
+  var im4 = document.getElementById("im4");
+  var im5 = document.getElementById("im5");
+  var im6 = document.getElementById("im6");
+  var im7 = document.getElementById("im7");
+  var im8 = document.getElementById("im8");
+  im1.setAttribute("src", "http://i.imgur.com/epPjEeG.gif");
+  im2.setAttribute("src", "http://i.imgur.com/t1H4KpW.gif");
+  im3.setAttribute("src", "http://i.imgur.com/LKOMfBD.gif");
+  im4.setAttribute("src", "http://i.imgur.com/LjE0mXp.gif");
+  im5.setAttribute("src", "http://i.imgur.com/sWQLakV.gif");
+  im6.setAttribute("src", "http://i.imgur.com/fNg3Apz.gif");
+  im7.setAttribute("src", "https://i.imgur.com/XMwTkC1.gif");
+  im8.setAttribute("src", "http://i.imgur.com/VPHqHOe.gif");
+});
+*/
